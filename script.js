f3fb5702a2cd0c2b621e1e1fa4de3442406eca93
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-02-20 19:09:13 +1300

    lesson10-5

--- script.js
+++ script.js
@@ -6,9 +6,6 @@ const body = document.querySelector('body');
 const bookName = document.querySelectorAll('h2 a');
 const chapters = document.querySelectorAll('li')
 
-
-
-
 books[0].before(books[1]);
 books[0].after(books[4]);
 books[4].after(books[3]);
@@ -19,6 +16,16 @@ adv.remove();
 body.style.backgroundImage = 'url(./image/new_bg.jpg)';
 
 bookName[4].textContent = 'Книга 3. this и Прототипы Объектов';
+
+chapters[3].after(chapters[6]);
+chapters[6].after(chapters[8]);
+chapters[9].after(chapters[2]);
+
+chapters[47].after(chapters[55]);
+chapters[50].after(chapters[48]);
+chapters[53].after(chapters[51]);
+
+
 
 console.log(books);
 console.log(chapters);
